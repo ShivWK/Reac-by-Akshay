b2a9cb8a1c835df56bcfd3e657751a9023f4f465
Admin <shivendrawk@gmail.com>
2024-09-13 00:27:50 +0530

    Ep-01

---
+++ App.js
@@ -0,0 +1,18 @@
+let element = React.createElement(
+    'div',
+    { className: "Parent", id: "mine" },
+    React.createElement(
+        "div",
+        { className: "Child" }, [
+            React.createElement("h1", { id: "mine" }, "Hi I'm Shivendra Dwivedi"),
+            React.createElement("h2", {}, "hi i'm second h2")
+        ]
+    )
+);
+
+// console.log(element)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+console.log(root)
+
+root.render(element)
